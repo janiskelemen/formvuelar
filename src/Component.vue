5a33630d43fdc987b1032b6efe049e1af903898f
Janis Kelemen <jak@spacemates.io>
2018-11-12 17:14:57 +0100

    bundle

---
+++ src/Component.vue
@@ -0,0 +1,17 @@
+<script>
+  import FvlForm from './components/FvlForm.vue';
+  import FvlInput from './components/FvlInput.vue';
+  import FvlSelect from './components/FvlSelect.vue';
+  import FvlRadio from './components/FvlRadio.vue';
+  import FvlCheckbox from './components/FvlCheckbox.vue';
+  import FvlSubmit from './components/FvlSubmit.vue';
+
+  export default {
+    FvlForm,
+    FvlInput,
+    FvlSelect,
+    FvlRadio,
+    FvlCheckbox,
+    FvlSubmit
+  };
+</script>
