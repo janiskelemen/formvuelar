9b1d63307e629e6a3d680259dce78abde7359c0a
Janis Kelemen <jak@spacemates.io>
2018-12-15 21:41:22 +0100

    example update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
   <div id="app" class="p-8 lg:p-0">
     <a
+      rel="no-referer"
+      class="absolute pin-r pin-t m-4 mt-6 mr-12"
+      href="https://twitter.com/intent/tweet?text=Check%20out%20FormVuelar%21%20Vue%20form%20components%20done%20right%21&hashtags=formVuelar%2Cvue&url=https%3A%2F%2Fjaniskelemen.github.io%2Fformvuelar&original_referer=https%3A%2F%2Ftwitter.com%2Fshare%3Ftext%3DCheck%2520out%2520FormVuelar%21%2520Vue%2520form%2520components%2520done%2520right%21%26hashtags%3DformVuelar%2Cvue%26url%3Dhttps%3A%2F%2Fjaniskelemen.github.io%2Fformvuelar"
+      title="Share on Twitter"
+      target="_blank"
+    >
+      <svg width="56px" height="22px" viewBox="328 355 335 276" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="
+    M 630, 425
+    A 195, 195 0 0 1 331, 600
+    A 142, 142 0 0 0 428, 570
+    A  70,  70 0 0 1 370, 523
+    A  70,  70 0 0 0 401, 521
+    A  70,  70 0 0 1 344, 455
+    A  70,  70 0 0 0 372, 460
+    A  70,  70 0 0 1 354, 370
+    A 195, 195 0 0 0 495, 442
+    A  67,  67 0 0 1 611, 380
+    A 117, 117 0 0 0 654, 363
+    A  65,  65 0 0 1 623, 401
+    A 117, 117 0 0 0 662, 390
+    A  65,  65 0 0 1 630, 425
+    Z"
+          style="fill:#3BA9EE;"
+        ></path>
+      </svg>
+    </a>
+    <a
       class="absolute pin-r pin-t m-4"
       href="https://github.com/janiskelemen/formvuelar"
       title="View on Github"
