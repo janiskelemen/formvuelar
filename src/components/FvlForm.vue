bd807daa183e48508a71db73a7ccd73ef9c811a1
Janis Kelemen <janis@helpspace.io>
2019-11-15 14:47:31 +0100

    convert booeans to numbers on multipart forms

--- src/components/FvlForm.vue
+++ src/components/FvlForm.vue
@@ -1,188 +1,189 @@
 <template>
   <form
-    :class="{'fvl-form-drag-over': isDragging}"
+    :class="{ 'fvl-form-drag-over': isDragging }"
     @submit.prevent="submit()"
-    @dragover="isDragging=true"
-    @dragend="isDragging=false"
-    @dragleave="isDragging=false"
-    @dragexit="isDragging=false"
-    @drop="isDragging=false"
+    @dragover="isDragging = true"
+    @dragend="isDragging = false"
+    @dragleave="isDragging = false"
+    @dragexit="isDragging = false"
+    @drop="isDragging = false"
   >
     <slot></slot>
   </form>
 </template>
 
 <script>
-  import axios from 'axios'
-  import _assignIn from 'lodash/assignIn'
-  import { config } from './mixins/config'
+import axios from 'axios'
+import _assignIn from 'lodash/assignIn'
+import { config } from './mixins/config'
 
-  export default {
-    mixins: [config],
-    props: {
-      method: {
-        type: String,
-        default: 'post',
-        validator: function(value) {
-          // The value must match one of the axios methods
-          return ['get', 'post', 'put', 'patch', 'delete'].indexOf(value) !== -1
-        }
-      },
-      url: {
-        type: String,
-        required: true
-      },
-      multipart: {
-        type: Boolean,
-        default: false
-      },
-      headers: {
-        type: Object,
-        default: () => {}
-      },
-      data: {
-        type: Object,
-        default: () => {
-          return {}
-        }
+export default {
+  mixins: [config],
+  props: {
+    method: {
+      type: String,
+      default: 'post',
+      validator: function(value) {
+        // The value must match one of the axios methods
+        return ['get', 'post', 'put', 'patch', 'delete'].indexOf(value) !== -1
       }
     },
-    data() {
-      return {
-        errors: {},
-        uploadPercentage: 0,
-        isLoading: false,
-        isDragging: false
-      }
+    url: {
+      type: String,
+      required: true
     },
-    computed: {
-      additionalHeaders() {
-        let multipart = this.multipart
-          ? {
-              'Content-Type': 'multipart/form-data'
-            }
-          : {}
-        let globalHeaders = this.getConfig('headers', {})
-        return _assignIn(globalHeaders, this.headers, multipart)
-      }
+    multipart: {
+      type: Boolean,
+      default: false
     },
-    methods: {
-      prepareData() {
-        let rawData = this.data
-        let formData = new FormData()
+    headers: {
+      type: Object,
+      default: () => {}
+    },
+    data: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  data() {
+    return {
+      errors: {},
+      uploadPercentage: 0,
+      isLoading: false,
+      isDragging: false
+    }
+  },
+  computed: {
+    additionalHeaders() {
+      let multipart = this.multipart
+        ? {
+            'Content-Type': 'multipart/form-data'
+          }
+        : {}
+      let globalHeaders = this.getConfig('headers', {})
+      return _assignIn(globalHeaders, this.headers, multipart)
+    }
+  },
+  methods: {
+    prepareData() {
+      let rawData = this.data
+      let formData = new FormData()
 
-        /* Map incoming data into formData */
-        Object.keys(rawData).forEach(e => {
-          if (rawData[e] instanceof Array) {
-            Object.keys(rawData[e]).forEach(f => {
-              if (rawData[e][f] instanceof File) {
-                formData.append(e + '[]', rawData[e][f])
-              } else {
-                formData.append(e, this.replaceNullOnMultipart(rawData[e]))
-              }
-            })
-          } else {
-            if (rawData[e] instanceof Object && !(rawData[e] instanceof File)) {
-              formData.append(e, JSON.stringify(this.replaceNullOnMultipart(rawData[e])))
+      /* Map incoming data into formData */
+      Object.keys(rawData).forEach(e => {
+        if (rawData[e] instanceof Array) {
+          Object.keys(rawData[e]).forEach(f => {
+            if (rawData[e][f] instanceof File) {
+              formData.append(e + '[]', rawData[e][f])
             } else {
               formData.append(e, this.replaceNullOnMultipart(rawData[e]))
             }
+          })
+        } else {
+          if (rawData[e] instanceof Object && !(rawData[e] instanceof File)) {
+            formData.append(e, JSON.stringify(this.replaceNullOnMultipart(rawData[e])))
+          } else if (this.multipart && typeof rawData[e] === 'boolean') {
+            formData.append(e, rawData[e] ? 1 : 0)
+          } else {
+            formData.append(e, this.replaceNullOnMultipart(rawData[e]))
           }
-        })
-
-        /* Append original method into form data */
-        if (this.method == 'patch' || this.method == 'put') {
-          formData.append('_method', this.method)
         }
+      })
 
-        return formData
-      },
-      replaceNullOnMultipart(val) {
-        return this.multipart && val === null ? '' : val
-      },
-      submit() {
-        let $this = this
-        this.isLoading = true
-        this.$emit('requeststarted')
-        /* Set method to post if multipart form was send via patch/put */
-        let method = this.multipart && (this.method == 'patch' || this.method == 'put') ? 'post' : this.method
-        this.loadInterceptors()
-        axios({
-          method: method,
-          url: this.url,
-          data: this.multipart ? this.prepareData() : this.data,
-          // only set params if request is get or delete
-          params: this.method == 'get' || this.method == 'delete' ? this.data : {},
-          headers: this.additionalHeaders,
+      /* Append original method into form data */
+      if (this.method == 'patch' || this.method == 'put') {
+        formData.append('_method', this.method)
+      }
 
-          // Calculate current upload percentage
-          onUploadProgress: function(progressEvent) {
-            let percentage = Math.round((progressEvent.loaded * 100) / progressEvent.total)
-            $this.uploadPercentage = percentage
-            $this.$emit('uploadProgress', percentage)
+      return formData
+    },
+    replaceNullOnMultipart(val) {
+      return this.multipart && val === null ? '' : val
+    },
+    submit() {
+      let $this = this
+      this.isLoading = true
+      this.$emit('requeststarted')
+      /* Set method to post if multipart form was send via patch/put */
+      let method = this.multipart && (this.method == 'patch' || this.method == 'put') ? 'post' : this.method
+      this.loadInterceptors()
+      axios({
+        method: method,
+        url: this.url,
+        data: this.multipart ? this.prepareData() : this.data,
+        // only set params if request is get or delete
+        params: this.method == 'get' || this.method == 'delete' ? this.data : {},
+        headers: this.additionalHeaders,
+
+        // Calculate current upload percentage
+        onUploadProgress: function(progressEvent) {
+          let percentage = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+          $this.uploadPercentage = percentage
+          $this.$emit('uploadProgress', percentage)
+        }
+      })
+        .then(function(response) {
+          $this.$emit('success', response)
+        })
+        .catch(function(error) {
+          /* Catch validation errors if status is 422 */
+          if (error.response && error.response.status === 422) {
+            $this.errors = error.response.data.errors
           }
+
+          /* Catch CSRF Token Missmatch - 419 */
+          if (error.response && error.response.status === 419) {
+            $this.$emit('error', { response: { status: 419 } })
+            return
+          }
+
+          /* Catch API Token Missmatch - 401 */
+          if (error.response && error.response.status === 401) {
+            $this.$emit('error', { response: { status: 401 } })
+            return
+          }
+
+          $this.$emit('error', error.response)
         })
-          .then(function(response) {
-            $this.$emit('success', response)
-          })
-          .catch(function(error) {
-            /* Catch validation errors if status is 422 */
-            if (error.response && error.response.status === 422) {
-              $this.errors = error.response.data.errors
-            }
+        .then(function() {
+          $this.isLoading = false
+          $this.uploadPercentage = 0
+          $this.$emit('uploadProgress', 0)
+          $this.$emit('requestfinished')
+        })
+    },
 
-            /* Catch CSRF Token Missmatch - 419 */
-            if (error.response && error.response.status === 419) {
-              $this.$emit('error', { response: { status: 419 } })
-              return
-            }
+    dirty(name) {
+      this.errors[name] = false
+    },
 
-            /* Catch API Token Missmatch - 401 */
-            if (error.response && error.response.status === 401) {
-              $this.$emit('error', { response: { status: 401 } })
-              return
-            }
+    getErrors(name) {
+      return this.errors[name] ? this.errors[name] : []
+    },
 
-            $this.$emit('error', error.response)
-          })
-          .then(function() {
-            $this.isLoading = false
-            $this.uploadPercentage = 0
-            $this.$emit('uploadProgress', 0)
-            $this.$emit('requestfinished')
-          })
-      },
+    hasErrors(name) {
+      return this.errors[name] && this.errors[name] !== [] ? true : false
+    },
+    loadInterceptors() {
+      let $this = this
 
-      dirty(name) {
-        this.errors[name] = false
-      },
+      // Add config interceptors
+      let interceptors = $this.getConfig('interceptors', false)
+      if (!interceptors) return
 
-      getErrors(name) {
-        return this.errors[name] ? this.errors[name] : []
-      },
-
-      hasErrors(name) {
-        return this.errors[name] && this.errors[name] !== [] ? true : false
-      },
-      loadInterceptors() {
-        let $this = this
-
-        // Add config interceptors
-        let interceptors = $this.getConfig('interceptors', false)
-        if (!interceptors) return
-
-        if (interceptors.request) {
-          axios.interceptors.request.use(interceptors.request.before, interceptors.request.error)
-        }
-        if (interceptors.response) {
-          axios.interceptors.response.use(interceptors.response.success, interceptors.response.error)
-        }
+      if (interceptors.request) {
+        axios.interceptors.request.use(interceptors.request.before, interceptors.request.error)
+      }
+      if (interceptors.response) {
+        axios.interceptors.response.use(interceptors.response.success, interceptors.response.error)
       }
     }
   }
+}
 </script>
 
 <style lang="scss">
-  @import '../assets/scss/formvuelar.scss';
+@import '../assets/scss/formvuelar.scss';
 </style>
-
