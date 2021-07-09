9d18514f34890b4ba3913babd5a66336575412d4
Janis Kelemen <janis@helpspace.io>
2021-07-09 15:22:53 +0200

    fix select chevron position

--- src/components/FvlSelect.vue
+++ src/components/FvlSelect.vue
@@ -7,6 +7,7 @@
     <div class="fvl-select">
       <select
         :id="id"
+        :ref="_uid"
         :name="name"
         :placeholder="placeholder"
         :autocomplete="autocomplete"
@@ -23,7 +24,10 @@
         <option v-if="allowEmpty" disabled selected value v-text="placeholder" />
         <option v-for="(option, key) in options" :key="key" :value="key" :selected="selected == key" v-text="option" />
       </select>
-      <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+      <div
+        class="pointer-events-none absolute h-full inset-y-0 right-0 flex items-center px-2 text-gray-700"
+        @click="$refs[_uid].click()"
+      >
         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
           <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path>
         </svg>
